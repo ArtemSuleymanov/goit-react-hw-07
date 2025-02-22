633e9c387986e7555395bd32d3ecab63eea1972d
Artem <suleymanov2007artem@gmail.com>
2025-02-22 14:46:36 +0200

    add contactsOps and add extrareducers in contactsSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
 
-const contactsPersistConfig = {
-  key: "contacts",
-  storage,
-  whitelist: ["items"],
-};
-
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(contactsPersistConfig, contactsReducer),
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
   middleware: (getDefaultMiddleware) =>
@@ -20,5 +12,3 @@ export const store = configureStore({
       serializableCheck: false,
     }),
 });
-
-export const persistor = persistStore(store);
